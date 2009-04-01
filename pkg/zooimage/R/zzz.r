7592e4bac4971515604459ef2bcd401852577cc0
Romain François <romain@thinkr.fr>
2009-04-01 12:37:28 +0000

    minor changes

--- pkg/zooimage/R/zzz.r
+++ pkg/zooimage/R/zzz.r
@@ -19,24 +19,37 @@
 # Loading and unloading ZooImage.
 # TODO: modify this since we have a NAMESPACE now
 
-".First.lib" <-
-function(libname, pkgname) {
+# {{{ Loading ZooImage
+".First.lib" <- function(libname, pkgname) {
+	
+	# {{{ check package dependencies
 	(require(utils) || stop("Package 'utils' is required!"))
 	(require(svMisc) || stop("Package 'svMisc' from SciViews bundle is required!"))
 	(require(svWidgets) || stop("Package 'svWidgets' is required!"))
 	(require(svDialogs) || stop("Package 'svDialogs' from SciViews bundle is required!"))
 	(require(tcltk2) || stop("Package 'tcltk2' is required!"))
-
-	# Use the SciViews style for dialog boxes
+	# }}}
+	
+	# {{{ check capabilities
+	checkZipAvailable() 
+	checkUnzipAvailable() 
+	checkZipnoteAvailable()
+	# }}}
+	
+	# {{{ Use the SciViews style for dialog boxes
 	options(guiStyle = "SciViews")
+	# }}}
 	
-	# Did we redefined the ZooImage config?
+	# {{{ Did we redefined the ZooImage config?
 	redef <- getOption("ZIredefine")
-	if (is.null(redef)) redef <- FALSE
+	if (is.null(redef)) {
+		redef <- FALSE
+	}
 	options(ZIredefine = NULL) 
+	# }}}
 	
-	# Create some strings in TempEnv
-	ZIversion <- "1.2-1"
+	# {{{ Create some strings in TempEnv
+	ZIversion <- packageDescription( "utils", field = "Version" )
 	assignTemp("ZIversion", ZIversion)
 
 	ZIname <- getTemp("ZIname")
@@ -58,79 +71,124 @@ function(libname, pkgname) {
 		#ZIgui <- file.path(libname, pkgname, "gui")
 	}
 	assignTemp("ZIgui", ZIgui)
+	# }}}
 	
+	# {{{ windows specific things
 	if (isWin()) {
 		ZIico <- getTemp("ZIico")
 		if (!redef || is.null(ZIgui))
 			ZIico <- tk2ico.create(file.path(getTemp("ZIgui"), "ZooImage.ico"))
 		assignTemp("ZIico", ZIico)
+	
 		# Make sure there is a key for ZooImage in the registry
 		ZIkey <- "HKEY_LOCAL_MACHINE\\Software\\ZooImage"
 		tk2reg.setkey(ZIkey)
 		assignTemp("ZIkey", ZIkey)
 	}
+	# }}}
 
-	# Load the various image resources
-	if (!redef) tkImgReadPackage("zooimage")
-	# Load the menus
+	# {{{ Load the various image resources
+	if (!redef){
+		tkImgReadPackage("zooimage")
+	}
+	# }}}
+	
+	# {{{ Load the menus
 	if (!redef) MenuReadPackage("zooimage")
-	# Possibly create the ZIguiPackage variable to indicate from where to load other GUI resources
+	# }}}
+	
+	# {{{ Possibly create the ZIguiPackage variable to indicate from where to load other GUI resources
 	ZIguiPackage <- getTemp("ZIguiPackage")
 	if (!redef || is.null(ZIguiPackage))	
 		ZIguiPackage <- "zooimage"
 	assignTemp("ZIguiPackage", ZIguiPackage)
+	# }}}
 
-	# Determine where to find the metadata editor
+	# {{{ Determine where to find the metadata editor
 	## TODO... currently, it is in a fixed position
-	bindir <- dirname(dirname(R.home()))
-	ZIEditorExe <- file.path(bindir, "MetaEditor", "Sc1.exe") # Note: only under Windows!
-	if (file.exists(ZIEditorExe))
-		options(ZIEditor = ZIEditorExe)
-
-	# Determine where to find ImageJ
+	if( isWin() ){
+		ZIEditorExe <- system.file( "MetaEditor", "Sc1.exe", package = "zooimage" )
+		if (file.exists(ZIEditorExe)){
+			options(ZIEditor = ZIEditorExe)
+		} else{
+			# fall back on the default editor
+			options( ZIEditor = getOption( "editor") )
+		}
+	} else{ 
+		options( ZIEditor = getOption( "editor") )
+	}
+	# }}}
+		
+	# {{{ Determine where to find ImageJ
 	## TODO... currently, it is in a fixed position
+	# TODO: no need to ship the exe file, we can just ship a simple
+	#       bat file with 
+	#       java -jar ij.jar -ijpath=./plugins
 	ImageJExe <- file.path(bindir, "ImageJ", "ImageJ.exe")
-	if (file.exists(ImageJExe))
-		options(ImageEditor = ImageJExe)
+	if( isWin() ){
+		if (file.exists(ImageJExe)){
+			options(ImageEditor = ImageJExe)
+		}
+	}
+	# }}}
 
-	# Determine where to find XnView
+	# {{{ Determine where to find XnView
 	## TODO... currently, it is in a fixed position
 	XnViewExe <- file.path(bindir, "XnView", "XnView.exe")
-	if (file.exists(XnViewExe))
-		options(ImageViewer = XnViewExe)
+	if( isWin() ){
+		if (file.exists(XnViewExe)){
+			options(ImageViewer = XnViewExe)
+		}
+	}
+	# }}}
 
-	# Determine where to find the zip viewer (Filzip under Windows)
+	# {{{ Determine where to find the zip viewer (Filzip under Windows)
 	## TODO... currently, it is in a fixed position
 	if (isWin()) {
 		FilzipExe <- file.path(bindir, "Filzip", "Filzip.exe")
-		if (file.exists(FilzipExe))
+		if (file.exists(FilzipExe)){
 			options(ZipViewer = FilzipExe)
+		}
+	} else{
+		
 	}
+	# }}}
 
-	# Determine where to find the DVD burner (DeepBurner under Windows)
+	# {{{ Determine where to find the DVD burner (DeepBurner under Windows)
 	## TODO... currently, it is in a fixed position
 	if (isWin()) {
 		DeepBurnerExe <- file.path(bindir, "DeepBurner", "DeepBurner.exe")
-		if (file.exists(DeepBurnerExe))
+		if (file.exists(DeepBurnerExe)){
 			options(DVDBurner = DeepBurnerExe)
+		}
 	}
+	# }}}
 
-	# Determine where to find XnView
+	# {{{ Determine where to find VueScan
 	## TODO... currently, it is in a fixed position
 	VueScanExe <- file.path(bindir, "VueScan", "VueScan.exe")
-	if (file.exists(VueScanExe))
-		options(VueScan = VueScanExe)
+	if( isWin() ){
+		if (file.exists(VueScanExe)){
+			options(VueScan = VueScanExe)
+		}
+	}
+	# }}}
 
-	# Possibly load the ZooImage assistant
+	# {{{ Possibly load the ZooImage assistant
 	LoadIt <- getOption("ZIAssistant")
 	if (is.null(LoadIt) || LoadIt == TRUE) ZIDlg()
+	# }}}
 	
-	# Switch to the default directory, if defined
+	# {{{ Switch to the default directory, if defined
 	defdir <- getKey("DefaultDirectory", "")
-    if (defdir != "" && file.exists(defdir) && file.info(defdir)$isdir)
+    if (defdir != "" && file.exists(defdir) && file.info(defdir)$isdir){
         Setwd(defdir)
+	}
+	# }}}
 }
+# }}}
 
+# {{{ Unloading ZooImage
 ".Last.lib" <-
 function(libpath) {
 	# Eliminate the ZooImage menu entries
@@ -142,3 +200,6 @@ function(libpath) {
 	# Destroy the ZooImage Tk window, if it is currently displayed
 	tkWinDelete("ZIDlgWin")	 
 }
+# }}}
+
+# :tabSize=4:indentSize=4:noTabs=false:folding=explicit:collapseFolds=1:
