ed28dc6384f64b2cca7d181a5810309993812eb2
Romain François <romain@thinkr.fr>
2009-06-04 09:54:35 +0000

    [bug] calling checkFileExists with named message argument

--- pkg/zooimage/R/utilities.r
+++ pkg/zooimage/R/utilities.r
@@ -871,7 +871,7 @@ mustbeString <- function( x, length){
 #' get a template file from the "ZITemplate" option
 template <- function( file = "default.zim", dir = getOption("ZITemplates") ){
 	f <- file.path( dir, file )
-	checkFileExists( f, "template file '%s' does not exist" )
+	checkFileExists( f, message = "template file '%s' does not exist" )
 	f
 }
 
